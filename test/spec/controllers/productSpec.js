35a7a77e303305131fc1746df06530d8f0c9d99d
fgcui <fgcui1204@163.com>
2014-08-29 14:21:58 +0800

    modify productSpec

--- test/spec/controllers/productSpec.js
+++ test/spec/controllers/productSpec.js
@@ -1,17 +1,36 @@
 /**
- * Created by fgcui on 14-8-28.
- */
-//describe("productCtrl",function(){
-//    var $scope,fromLocal,productService;
-//    beforeEach(function () {
-//        module('ngLetusgoApp');
-//        inject(function ($injector){
-//            $scope = $injector.get('$rootScope').$new();
-//            fromLocal = $injector.get('fromLocal');
-//            productService = $injector.get('productService');
-//        });
-//    });
-//    it ('should has start date', function () {
-//        expect(forms[3].fields[0].value).toEqual('2014-05-31');
-//    });
-//});
+* Created by fgcui on 14-8-28.
+*/
+describe("productCtrl",function(){
+    var $scope,fromLocal,productService,createController;
+    beforeEach(function () {
+        module('ngLetusgoApp');
+        inject(function ($injector){
+            $scope = $injector.get('$rootScope').$new();
+            fromLocal = $injector.get('fromLocal');
+            productService = $injector.get('productService');
+            var $controller = $injector.get('$controller');
+
+            createController = function () {
+            return $controller('ProCtrl', {
+              $scope: $scope,
+              fromLocal: fromLocal,
+              productService: productService
+            });
+          };
+
+        });
+    });
+    it ('setToLocal should be execute', function () {
+      var allProduct = [{p_sort:'水果',p_name:'苹果',p_price:'10',p_unit:'千克'},
+        {p_sort:'水果',p_name:'香蕉',p_price:'5',p_unit:'千克'}]
+        spyOn(productService,'setToLocal');
+        spyOn(productService,'addToCart');
+        spyOn(fromLocal,'getData').andReturn(allProduct);
+        createController();
+        $scope.addToCart(allProduct[0]);
+        expect($scope.products.length).toBe(2);
+        expect(productService.addToCart.calls.length).toBe(1);
+        expect(productService.setToLocal.calls.length).toBe(1);
+    });
+});
