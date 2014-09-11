20581aec6b2a4092d5fd6f3815839bcab5975d7d
fgcui <fgcui1204@163.com>
2014-09-11 21:07:33 +0800

    modify doUpdate

--- app/scripts/controllers/updateProduct.js
+++ app/scripts/controllers/updateProduct.js
@@ -2,15 +2,16 @@
  * Created by fgcui on 14-9-7.
  */
 angular.module('ngLetusgoApp')
-  .controller('updateProduct', function ($scope,fromLocal,productManagerService,$location) {
-//    $scope.allSorts = productManagerService.getAllSort();
-   // $scope.productInfo = productManagerService.getProductInfo();
-//    $scope.productInfo = productManagerService.productInfo();
-//    $scope.addProduct = function(){
-//      var productInfo = $scope.productInfo;
-//      var allProducts = fromLocal.getData("allProduct");
-//      allProducts.push(productInfo);
-//      fromLocal.setData("allProduct",allProducts);
-//      $location.path('/productManager');
-//    }
+  .controller('updateProduct', function ($scope,fromLocal,productManagerService,$routeParams,$location) {
+    $scope.allSorts = productManagerService.getAllSort();
+    var item = productManagerService.getProductByName($routeParams.name);
+    $scope.productInfo = {'p_sort':item[0].p_sort,p_name:item[0].p_name,p_price:item[0].p_price,p_unit:item[0].p_unit};
+
+    $scope.doUpdate = function(){
+      productManagerService.delete($scope.productInfo.p_name);
+      var allProducts = fromLocal.getData("allProduct");
+      allProducts.push($scope.productInfo);
+      fromLocal.setData("allProduct",allProducts);
+      $location.path('/productManager');
+    }
   });
