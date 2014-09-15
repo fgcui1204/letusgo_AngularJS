3d34cc8b2f43166062d63a933af5cf4a6d8c5c69
fgcui <fgcui1204@163.com>
2014-09-15 15:21:29 +0800

    delete the date

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name ngLetusgoApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the ngLetusgoApp
- */
 angular.module('ngLetusgoApp')
   .controller('MainCtrl', function ($scope, $location, productService) {
 
