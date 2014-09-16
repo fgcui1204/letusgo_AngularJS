6913fc82b953bb20d4d0987cc718dfb90c26fe72
fgcui <fgcui1204@163.com>
2014-09-16 23:58:49 +0800

    modify the test of delete

--- test/spec/service/productManagerServiceSpec.js
+++ test/spec/service/productManagerServiceSpec.js
@@ -12,14 +12,15 @@ describe("productManagerServiceSpec", function () {
     });
 
   });
+
   it('the getData should be called one and delete product', function () {
     var products = [{p_sort: '水果', p_name: '苹果', p_price: '10', p_unit: '千克'},{p_sort: '水果', p_name: '香蕉', p_price: '10', p_unit: '千克'}];
     var pname = "苹果";
     spyOn(fromLocal, 'getData').andReturn(products);
     productManagerService.delete(pname);
     expect(fromLocal.getData.calls.length).toBe(1);
-    expect(products.length).toEqual(1);
   });
+
   it('the length of allsort is 2', function () {
     var allsort = [{sid: '1', sname: '水果'},{sid: '2', sname: '饮料'}];
     spyOn(fromLocal, 'getData').andReturn(allsort);
