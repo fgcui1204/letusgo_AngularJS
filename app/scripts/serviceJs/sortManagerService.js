c3dcd5716612e019d8438d75c7efb1293c1e66bf
fgcui <fgcui1204@163.com>
2014-09-16 11:52:16 +0800

    refactor the sortManagerSErvice

--- app/scripts/serviceJs/sortManagerService.js
+++ app/scripts/serviceJs/sortManagerService.js
@@ -1,10 +1,11 @@
 angular.module('ngLetusgoApp').service('sortManagerService', function (fromLocal,$location) {
+  var allSorts = fromLocal.getData('allSort');
   this.getAllSorts = function (){
     return fromLocal.getData('allSort');
   };
 
   this.delete = function(sort){
-    var allSorts = fromLocal.getData('allSort');
+
     var afterDeleteSorts = _.filter(allSorts, function (sorts) {
       return sorts.sname != sort.sname;
     });
@@ -14,11 +15,9 @@ angular.module('ngLetusgoApp').service('sortManagerService', function (fromLocal
     $location.path('/updateSort/'+sort.sid);
   };
   this.getSortById = function(id){
-    var allSorts = fromLocal.getData('allSort');
     return _.filter(allSorts, { 'sid': id });
   }
   this.doUpdate = function(sort){
-    var allSorts = fromLocal.getData('allSort');
     _.forEach(allSorts,function(sorts){
       if(sorts.sid == sort.sid){
         sorts.sname = sort.sname;
