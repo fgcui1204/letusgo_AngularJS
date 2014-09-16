e49104757e57960213a0dd2d6002f483f4972329
fgcui <fgcui1204@163.com>
2014-09-16 23:45:30 +0800

    modify the cartItem define

--- app/scripts/serviceJs/cartService.js
+++ app/scripts/serviceJs/cartService.js
@@ -1,13 +1,10 @@
 angular.module('ngLetusgoApp').service('cartService', function (fromLocal, productService) {
-  var cartItem = fromLocal.getData('cartProduct');
   this.cart_Item_count_not_0 = [];
 
   this.getTotalMoney = function () {
-
+    var cartItem = fromLocal.getData('cartProduct');
     var totalMoney = 0;
-    if (cartItem == null) {
-      totalMoney = 0;
-    } else {
+    if (cartItem !== null) {
       _.forEach(cartItem, function (item) {
         totalMoney += item.p_price * item.count;
       });
@@ -16,16 +13,20 @@ angular.module('ngLetusgoApp').service('cartService', function (fromLocal, produ
   };
 
   this.changeCount = function (item) {
+    var cartItem = fromLocal.getData('cartProduct');
+    this.cart_Item_count_not_0 = [];
     _.forEach(cartItem, function (cart_item) {
       if (cart_item.p_name == item.p_name) {
         cart_item.count = item.count;
       }
     });
+
     this.cart_Item_count_not_0 = _.filter(cartItem, function (item) {
       return item.count != 0;
     });
+
     fromLocal.setData('cartProduct', this.cart_Item_count_not_0);
     fromLocal.setData('totalCount', productService.getTotalCount());
-  }
+  };
 
 });
