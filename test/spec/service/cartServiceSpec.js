bdf7e062e16c9b2d58645532daa858d804df4a0a
fgcui <fgcui1204@163.com>
2014-09-17 20:10:09 +0800

    modify cartSErvice test

--- test/spec/service/cartServiceSpec.js
+++ test/spec/service/cartServiceSpec.js
@@ -2,7 +2,7 @@
  * Created by fgcui on 14-8-28.
  */
 describe("CartService", function () {
-  var cartService,fromLocal, productService;
+  var cartService,fromLocal, productService,cartItem;
   beforeEach(function () {
     module('ngLetusgoApp');
 
@@ -11,6 +11,11 @@ describe("CartService", function () {
       productService = $injector.get("productService");
       fromLocal = $injector.get('fromLocal');
     });
+    cartItem = [
+      {p_sort:'水果',p_name:'苹果',p_price:'10',p_unit:'千克',count: 3},
+      {p_sort:'饮料',p_name:'雪碧',p_price:'3',p_unit:'瓶', count: 3}
+    ];
+
 
   });
   it('total money should be 0',function(){
@@ -21,26 +26,23 @@ describe("CartService", function () {
   });
 
   it('total money should be 29',function(){
-    var cartItem = [{p_sort: '水果', p_name: '苹果', p_price: '10', p_unit: '千克', count: 2},{p_sort:'饮料',p_name:'雪碧',p_price:'3',p_unit:'瓶', count: 3}];
     spyOn(fromLocal,'getData').andReturn(cartItem);
     var totalMoney = cartService.getTotalMoney();
     expect(fromLocal.getData.calls.length).toBe(1);
-    expect(totalMoney).toEqual(29);
+    expect(totalMoney).toEqual(39);
   });
 
-  xit('when change the count in cart the setData should be called',function(){
-    var cartItem = [{p_sort: '水果', p_name: '苹果', p_price: '10', p_unit: '千克', count: 3},{p_sort:'饮料',p_name:'雪碧',p_price:'3',p_unit:'瓶', count: 3}];
+  it('when change the count in cart the setData should be called',function(){
     var item = {p_sort: '水果', p_name: '苹果', p_price: '10', p_unit: '千克',count: 4};
     spyOn(fromLocal,'getData').andReturn(cartItem);
     spyOn(fromLocal,'setData');
     cartService.changeCount(item);
     expect(fromLocal.setData.calls.length).toBe(2);
-    expect(fromLocal.getData.calls.length).toBe(1);
-    expect(cartItem[0].count).toEqual(3);
+    expect(fromLocal.getData.calls.length).toBe(2);
+    expect(cartItem[0].count).toEqual(4);
   });
 
-  xit('when the count is 0,cartProduct should delete this product',function(){
-    var cartItem = [{p_sort: '水果', p_name: '苹果', p_price: '10', p_unit: '千克', count: 1},{p_sort:'饮料',p_name:'雪碧',p_price:'3',p_unit:'瓶', count: 3}];
+  it('when the count is 0,cartProduct should delete this product',function(){
     var item = {p_sort: '水果', p_name: '苹果', p_price: '10', p_unit: '千克',count: 0};
     spyOn(fromLocal,'getData').andReturn(cartItem);
     spyOn(fromLocal,'setData');
