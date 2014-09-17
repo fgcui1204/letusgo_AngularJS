98d4802132e967e1efb6e7fc05696c7fa26fbd76
fgcui <fgcui1204@163.com>
2014-09-17 21:14:56 +0800

    test the location of to add

--- test/spec/controllers/productManagerSpec.js
+++ test/spec/controllers/productManagerSpec.js
@@ -1,19 +1,21 @@
 'use strict';
 
 describe("ProductManagerCtrl",function() {
-  var $scope, fromLocal, productManagerService, createController,allProduct;
+  var $scope, fromLocal, productManagerService,$location, createController,allProduct;
   beforeEach(function () {
     module('ngLetusgoApp');
     inject(function ($injector) {
       $scope = $injector.get('$rootScope').$new();
       fromLocal = $injector.get('fromLocal');
       productManagerService = $injector.get('productManagerService');
+      $location = $injector.get('$location')
       var $controller = $injector.get('$controller');
 
       createController = function () {
         return $controller('ProductManagerCtrl', {
           $scope: $scope,
           fromLocal: fromLocal,
+          $location:$location,
           productManagerService: productManagerService
         });
       };
@@ -38,7 +40,11 @@ describe("ProductManagerCtrl",function() {
     var pname = '苹果';
     $scope.delete(pname);
     expect(productManagerService.delete).toHaveBeenCalledWith(pname);
-    
   });
 
+  it('should come into addProduct when click the add product', function () {
+    createController();
+    $scope.toAdd();
+    expect($location.path() === '/addProduct').toBe(true);
+  });
 });
